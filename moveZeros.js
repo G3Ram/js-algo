db6c7f706b019371bc7025e75569b07cbb5e41d4
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-13 19:16:06 -0500

    moveZeros second atttempt

--- moveZeros.js
+++ moveZeros.js
@@ -3,12 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let numsLen = nums.length;
-  nums = nums.filter((i) => i !== 0);
-  while (nums.length < numsLen) {
-    nums.push(0);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
   }
-  console.log(nums);
 };
 
-moveZeroes([0, 1, 0, 3, 12]);
+moveZeroes([0, 0, 1]);
