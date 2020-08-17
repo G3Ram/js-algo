2ef3f0be0c4d65925f6881a8564ba9441933e8d2
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-17 18:40:29 -0500

    Solution 2

--- removeVowels.js
+++ removeVowels.js
@@ -2,12 +2,24 @@
  * @param {string} S
  * @return {string}
  */
+// var removeVowels = function (S) {
+//   let strArr = S.split("");
+//   strArr = strArr.filter((val) => {
+//     return !["a", "e", "i", "o", "u"].includes(val.toLowerCase());
+//   });
+//   return strArr.join().replace(/,/g, "");
+// };
+
+// console.log(removeVowels("podaanga"));
+
+// SOLUTION 2
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
 var removeVowels = function (S) {
-  let strArr = S.split("");
-  strArr = strArr.filter((val) => {
-    return !["a", "e", "i", "o", "u"].includes(val.toLowerCase());
-  });
-  return strArr.join().replace(/,/g, "");
+  return [...S].filter((val) => !"aeiou".includes(val.toLowerCase())).join("");
 };
 
 console.log(removeVowels("podaanga"));
