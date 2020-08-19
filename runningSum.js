7c3d5fa327dad77ad7dea7536a600720f9f7996b
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-19 18:10:33 -0500

    Running sum 1d array

---
+++ runningSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  let newArr = [];
+  let temp;
+  if (nums.length > 0) {
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+      total += nums[i];
+      newArr.push(total);
+    }
+    return newArr;
+  }
+  return nums;
+};
+
+console.log(runningSum([1, 2, 3]));
