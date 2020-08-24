643d492e6ad59d25d18b51bcc39510e8dccb6da8
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-24 18:43:23 -0500

    Two sum - 29 of 29 tests passed

--- twoSum.js
+++ twoSum.js
@@ -4,12 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  let indexVal = -1;
   for (let i = 0; i < nums.length; i++) {
-    if (nums.indexOf(target - nums[i]) !== -1) {
+    indexVal = nums.indexOf(target - nums[i]);
+    if (indexVal !== -1 && indexVal !== i) {
       return [i, nums.indexOf(target - nums[i])];
     }
   }
   return nums;
 };
 
-console.log(twoSum([1, 2, 3, 4, 5], 6));
+console.log(twoSum([3, 2, 4], 6));
