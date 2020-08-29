3fef8eb007d825680394a7d6d78b986708de8cea
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-29 08:12:09 -0500

    check possibility - second attempt

--- checkPossibility.js
+++ checkPossibility.js
@@ -4,13 +4,18 @@
  */
 var checkPossibility = function (nums) {
   let nonDecIndex = [];
+
   for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] > nums[i + 1]) {
-      nonDecIndex.push(i);
+    for (let j = i + 1; j < nums.length; j++) {
+      console.log(`-----${nums[i]}---------${nums[j]}`);
+      if (nums[j] - nums[i] < 0) {
+        nonDecIndex.push(nums[j]);
+        break;
+      }
     }
   }
-
+  console.log(nonDecIndex.length);
   return nonDecIndex.length <= 1;
 };
 
-console.log(checkPossibility([3, 4, 2, 3]));
+console.log(checkPossibility([5, 7, 1, 8]));
