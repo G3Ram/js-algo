6ba28de43dc4b701d36bba7759821b9af380c0bf
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-31 07:04:48 -0500

    Second solution to remove duplicates

--- duplicates.js
+++ duplicates.js
@@ -2,9 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var removeDuplicates = function (nums) {
+//   console.log(nums);
+//   const arr = [...new Set([...nums])];
+//   return arr;
+// };
+
 var removeDuplicates = function (nums) {
-  console.log(nums);
-  const arr = [...new Set([...nums])];
+  const arr = nums;
+  for (let i = 1; i < nums.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      arr.splice(i, 1);
+    }
+  }
   return arr;
 };
 
