0c8b54363cc7031592f1de49c3482f11ef625849
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-01 07:18:01 -0500

    Max profile - first try

--- duplicates.js
+++ duplicates.js
@@ -8,14 +8,26 @@
 //   return arr;
 // };
 
+// var removeDuplicates = function (nums) {
+//   const arr = nums;
+//   for (let i = 1; i < nums.length; i++) {
+//     if (arr[i] === arr[i - 1]) {
+//       arr.splice(i, 1);
+//     }
+//   }
+//   return arr;
+// };
+
 var removeDuplicates = function (nums) {
-  const arr = nums;
-  for (let i = 1; i < nums.length; i++) {
-    if (arr[i] === arr[i - 1]) {
-      arr.splice(i, 1);
+  let i = 0;
+  if (nums.length === 0) return 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i += 1;
+      nums[i] = nums[j];
     }
   }
-  return arr;
+  return i + 1;
 };
 
 console.log(removeDuplicates([1, 1, 2]));
