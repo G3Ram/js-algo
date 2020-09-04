46f7a501e6dc78e997d8bb286f367956350dc57f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-03 21:22:02 -0500

    Max profile - first successful submission

--- maxProfit.js
+++ maxProfit.js
@@ -4,21 +4,24 @@
  */
 var maxProfit = function (prices) {
   let maxPrice = 0;
-  let maxPriceArr = [];
-  let endOfPrices = false;
-  let temp = [];
-  for (let i = 0; i < prices.length; i++) {
-    if (temp.length !== 0) {
-      maxPriceArr.push(temp);
+  let temp = 0;
+  let count = 1;
+  let arr = [];
+  do {
+    for (let i = 1; i < prices.length; i = i + count) {
+      temp = prices[i] - prices[i - 1];
+      maxPrice = temp > 0 ? maxPrice + temp : maxPrice;
     }
-    temp = [];
-    for (let j = 0; j < prices.length; j++) {
-      maxPrice = prices[j] - prices[i];
-      temp.push(maxPrice);
-    }
-  }
-  maxPriceArr.push(temp);
-  return maxPriceArr;
+    arr.push(maxPrice);
+    maxPrice = 0;
+    count = count + 1;
+  } while (count < prices.length);
+  arr.sort(function (a, b) {
+    return b - a;
+  });
+  return arr[0];
 };
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+// console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+// console.log(maxProfit([1, 2, 3, 4, 5]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
