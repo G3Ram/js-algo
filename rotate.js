a305bf52c81120c6b54980f740fe020a96f51f37
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-07 21:34:48 -0500

    Second test case - success

--- rotate.js
+++ rotate.js
@@ -14,4 +14,5 @@ var rotate = function (nums, k) {
   return nums;
 };
 
-console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+console.log(rotate([-1, -100, 3, 99], 2));
