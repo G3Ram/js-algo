c7a99cb0bf150bddd740ba974ccb7eba7d60902b
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-08 21:52:12 -0500

    Super elegant solution

--- rotate.js
+++ rotate.js
@@ -3,13 +3,25 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var rotate = function (nums, k) {
+//   for (let i = 0; i < k; i++) {
+//     temp = nums[nums.length - 1];
+//     for (let j = nums.length - 1; j > 0; j--) {
+//       nums[j] = nums[j - 1];
+//     }
+//     nums[0] = temp;
+//   }
+//   return nums;
+// };
+
 var rotate = function (nums, k) {
+  let tmp = 0;
+  const len = nums.length;
+  k %= len;
+
   for (let i = 0; i < k; i++) {
-    temp = nums[nums.length - 1];
-    for (let j = nums.length - 1; j > 0; j--) {
-      nums[j] = nums[j - 1];
-    }
-    nums[0] = temp;
+    tmp = nums.pop();
+    nums.unshift(tmp);
   }
   return nums;
 };
