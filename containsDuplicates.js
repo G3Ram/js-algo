65d987a81268910611fe1476a2627095dc428f11
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-09 21:56:44 -0500

    Super elegant solution

---
+++ containsDuplicates.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+// console.log(containsDuplicate([1, 2, 3, 4]));
+// console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
