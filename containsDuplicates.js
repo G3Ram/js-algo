a65123460476ad57ae62a024b25dbabb1bd3eb60
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-10 22:00:10 -0500

    Beats 78% of the submission - yay

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -2,8 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+  const numsLen = nums.length;
+  nums = [...new Set(nums)];
+  return nums.length !== numsLen;
+};
 
-console.log(containsDuplicate([1, 2, 3, 1]));
-// console.log(containsDuplicate([1, 2, 3, 4]));
-// console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+// console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
