288f94698ad232c677be2cd9727417d4e8235264
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-12 08:49:49 -0500

    topKFrequent - first solution

--- topKFrequent.js
+++ topKFrequent.js
@@ -4,8 +4,21 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-  let newKSet = [...new Set(nums)];
-  console.log(newKSet);
+  let temp = {};
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (!result.includes(nums[i])) {
+      if (temp[nums[i].toString()]) {
+        temp[nums[i]] = temp[nums[i]] + 1;
+        if (temp[nums[i]] >= k) {
+          result.push(nums[i]);
+        }
+      } else {
+        temp[nums[i]] = 1;
+      }
+    }
+  }
+  console.log(result);
 };
 
 topKFrequent([1, 1, 1, 2, 2, 3], 2);
