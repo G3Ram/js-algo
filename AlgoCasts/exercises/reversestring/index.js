40caf32cb9f40e7ffdca3519b540dda30726e95e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-24 20:36:55 -0500

    Reverse a string second pass

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -7,12 +7,15 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  str = str.split("");
-  let result = [];
-  for (let i = str.length - 1; i > -1; i--) {
-    result.push(str[i]);
-  }
-  return result.join("");
+  // SOLUTION 1
+  //   str = str.split("");
+  //   let result = [];
+  //   for (let i = str.length - 1; i > -1; i--) {
+  //     result.push(str[i]);
+  //   }
+  //   return result.join("");
+
+  return str.split("").reverse().join("");
 }
 
 module.exports = reverse;
