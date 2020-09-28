f5446113182f7da181c77767e7f38e6274a328f6
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-09-27 20:51:36 -0500

    Reverse a string fourth pass

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -7,21 +7,25 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  // SOLUTION 3
-  let result = "";
-  for (let character of str) {
-    result = character + result;
-  }
-  return result;
-  // SOLUTION 1
-  //   str = str.split("");
-  //   let result = [];
-  //   for (let i = str.length - 1; i > -1; i--) {
-  //     result.push(str[i]);
-  //   }
-  //   return result.join("");
-  //   SOLUTION 2
-  // return str.split("").reverse().join("");
+  return str.split("").reduce((rev, char) => char + rev, "");
 }
 
 module.exports = reverse;
+
+// SOLUTION 1
+//   str = str.split("");
+//   let result = [];
+//   for (let i = str.length - 1; i > -1; i--) {
+//     result.push(str[i]);
+//   }
+//   return result.join("");
+
+//   SOLUTION 2
+// return str.split("").reverse().join("");
+
+// SOLUTION 3
+//   let result = "";
+//   for (let character of str) {
+//     result = character + result;
+//   }
+//   return result;
