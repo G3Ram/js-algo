adf646b8673f67ef1a62531dca2994364abc521c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-10-02 16:00:15 -0500

    Reverse int second pass

--- AlgoCasts/exercises/reverseint/index.js
+++ AlgoCasts/exercises/reverseint/index.js
@@ -9,13 +9,17 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  const nArr = n.toString().split("");
-  if (n > 0) {
-    return parseInt(nArr.reduce((rev, item) => item + rev, "")) || 0;
-  } else {
-    nArr.shift();
-    return parseInt(`-${nArr.reduce((rev, item) => item + rev, "")}`) || 0;
-  }
+  const reversed = n.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
+
+// SOLUTION 1
+//   const nArr = n.toString().split("");
+//   if (n > 0) {
+//     return parseInt(nArr.reduce((rev, item) => item + rev, "")) || 0;
+//   } else {
+//     nArr.shift();
+//     return parseInt(`-${nArr.reduce((rev, item) => item + rev, "")}`) || 0;
+//   }
