5ae42fa34db3a93bca6b698f6f5e49e809e01af9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-10-08 19:00:33 -0500

    Anagram first pass

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,19 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const charAVals = {};
+  const charBVals = {};
+  if (stringA.length !== stringB.length) return false;
+  for (let i = 0; i < stringA.length; i++) {
+    charAVals[stringA[i]] = charAVals[stringA[i]] + 1 || 1;
+    charBVals[stringB[i]] = charBVals[stringB[i]] + 1 || 1;
+  }
+
+  for (let charA in charAVals) {
+    if (charAVals[charA] !== charBVals[charA]) return false;
+  }
+  return true;
+}
 
 module.exports = anagrams;
