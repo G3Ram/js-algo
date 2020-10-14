7901c43ca0723fbec32ee766e7e0b3b437ee121e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-10-14 09:21:28 -0500

    vowels first pass

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowels = {};
+  for (let char of str) {
+    if (char.match(/[aeiouAEIOU]/g)) {
+      vowels[char] = vowels[char] ? vowels[char] + 1 : 1;
+    }
+  }
+
+  let result = 0;
+  for (let char in vowels) {
+    result += vowels[char];
+  }
+  return result;
+}
 
 module.exports = vowels;
