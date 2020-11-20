a6258fb1166f6f0237a157468b8c5e5d98c7ce3f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-11-19 20:39:22 -0600

    Plus one -  first pass

---
+++ LeetCode/plusOne.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  const dlen = digits.length;
+  let carryOver = 0;
+  for (let i = dlen - 1; i > -1; i--) {
+    if (digits[i] === 9 && i === dlen - 1) {
+      digits[i] = 0;
+      carryOver = 1;
+      if (digits.length === 1) {
+        digits.unshift(1);
+      }
+    } else if (digits[i] !== 9 && i === dlen - 1) {
+      digits[i] = digits[i] + 1;
+      carryOver = 0;
+    } else if (digits[i] === 9 && i === 0 && carryOver === 1) {
+      digits[i] = 0;
+      digits.unshift(1);
+      carryOver = 0;
+    } else if (digits[i] === 9 && carryOver === 1) {
+      digits[i] = 0;
+      carryOver = 1;
+    } else if (digits[i] !== 9 && carryOver === 1) {
+      digits[i] = digits[i] + 1;
+      carryOver = 0;
+    }
+  }
+
+  return digits;
+};
+
+console.log(plusOne([9]));
